24c80a70447f0429b985c798279e6402f121ee7b
Shohag Faraji <shohagfaraji2@gmail.com>
2025-08-14 00:39:51 +0600

    Add line with circles centered at both endpoints

---
+++ Lab_2/line_with_circles_at_ends.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+#include <GL/glut.h>
+#include <cmath>
+using namespace std;
+
+int x_start, y_start;
+int x_end, y_end;
+int radius;
+
+void putPixel(int x, int y) {
+    glBegin(GL_POINTS);
+    glVertex2i(x, y);
+    glEnd();
+}
+
+void drawBoldPixel(int x, int y) {
+    putPixel(x, y);
+}
+
+void drawLine8Neighbour(int x1, int y1, int x2, int y2) {
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = (x2 >= x1) ? 1 : -1;
+    int sy = (y2 >= y1) ? 1 : -1;
+
+    int err = dx - dy;
+
+    while (true) {
+        drawBoldPixel(x1, y1);
+        if (x1 == x2 && y1 == y2) break;
+        int e2 = (err << 1);
+        if (e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
+
+void drawCircle(int cx, int cy, int r) {
+    int x = 0, y = r;
+    int d = 3 - (r << 1);
+
+    while (x <= y) {
+        putPixel(cx + x, cy + y);
+        putPixel(cx - x, cy + y);
+        putPixel(cx + x, cy - y);
+        putPixel(cx - x, cy - y);
+        putPixel(cx + y, cy + x);
+        putPixel(cx - y, cy + x);
+        putPixel(cx + y, cy - x);
+        putPixel(cx - y, cy - x);
+        if (d < 0) {
+            d += (x << 2) + 6;
+        } else {
+            d += ((x - y) << 2) + 10;
+            y--;
+        }
+        x++;
+    }
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.1, 0.1, 0.1);
+
+    drawLine8Neighbour(x_start, y_start, x_end, y_end);
+
+    glColor3f(1.0, 0.0, 0.0);
+    drawCircle(x_start, y_start, radius);
+
+    glColor3f(0.0, 0.0, 1.0);
+    drawCircle(x_end, y_end, radius);
+
+    glFlush();
+}
+
+void init() {
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    gluOrtho2D(0, 500, 0, 500);
+    glPointSize(1.0);
+}
+
+int main(int argc, char** argv) {
+     cout << "Enter start point (x1, y1): ";
+    cin >> x_start >> y_start;
+
+    cout << "Enter End point (x2, y2): ";
+    cin >> x_end >> y_end;
+
+    cout << "Enter Radius : ";
+    cin >> radius;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Bold Line with Circles");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+}
